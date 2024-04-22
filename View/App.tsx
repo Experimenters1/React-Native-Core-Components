c124d3daa26e98ed7b84a913ff1fd24bd2b06ec3
Huy vu <huyvu@Huys-MacBook-Pro.local>
2024-04-22 13:22:06 +0700

    Frist one

--- View/App.tsx
+++ View/App.tsx
@@ -5,33 +5,60 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+
 } from 'react-native';
 
-function App(): React.JSX.Element {
 
+
+function App(): React.JSX.Element {
+ 
   return (
-    <View style={styles.container}>
-      {/* Other components will be placed here */}
-    </View>
+       <View style={styles.container}>
+          <View style={styles.box}>
+             <Text style={styles.Text}>Hello Worlds</Text>
+          </View>
+       </View>
   );
+
+  
 }
 
 const styles = StyleSheet.create({
   container: {
-       flex: 1,
+       flex: 1, // Cho phép thẻ View sử dụng toàn bộ không gian màn hình
        justifyContent: 'center',
-       alignContent: 'center',
-       backgroundColor: '#960e0e',
-  }
+       alignContent:'center',
+      backgroundColor: '#97b2de',
+      padding: 10,
+  },
+  box: {
+     backgroundColor: '#007AFF',
+     padding: 10,
+     borderRadius: 45,
+     height: 350,
+     width: 350,
+     justifyContent: 'center',
+     alignItems: 'center',
+     marginLeft: 15
+  },
+
+  Text: {
+    backgroundColor: '#fff',
+    height: 40,
+    textAlign: 'center',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  
 });
 
 export default App;
+
+//https://chat.openai.com/c/ad5080fe-ab1c-4622-bffc-90e185b7de7f
