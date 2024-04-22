f9fdc4876c7019513f4b818fbe3f87c88948e7d3
Huy vu <huyvu@Huys-MacBook-Pro.local>
2024-04-22 13:37:53 +0700

    Second commit

---
+++ View/onLayout.tsx
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  LayoutChangeEvent  // Import LayoutChangeEvent
+
+} from 'react-native';
+
+
+
+
+function App(): React.JSX.Element {
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    const {x, y, width, height} = event.nativeEvent.layout;
+    console.log(`Layout changed. x: ${x}, y: ${y}, width: ${width}, height: ${height}`);
+  };
+ 
+  return (
+       <View style={styles.container} onLayout={handleLayout}>
+          <View style={styles.box}>
+             <Text style={styles.Text}>Hello Worlds</Text>
+          </View>
+       </View>
+  );
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+       flex: 1, // Cho phép thẻ View sử dụng toàn bộ không gian màn hình
+       justifyContent: 'center',
+       alignContent:'center',
+      backgroundColor: '#97b2de',
+      padding: 10,
+  },
+  box: {
+     backgroundColor: '#007AFF',
+     padding: 10,
+     borderRadius: 45,
+     height: 350,
+     width: 350,
+     justifyContent: 'center',
+     alignItems: 'center',
+     marginLeft: 15
+  },
+
+  Text: {
+    backgroundColor: '#fff',
+    height: 40,
+    textAlign: 'center',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  
+});
+
+export default App;
+
+//https://chat.openai.com/c/ad5080fe-ab1c-4622-bffc-90e185b7de7f
