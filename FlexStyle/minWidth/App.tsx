58ff1f8f11f6a52777ae42a736d1781c78729cc0
Huy vu <huyvu@Huys-MacBook-Pro.local>
2024-04-23 11:22:51 +0700

    Second commit

---
+++ FlexStyle/minWidth/App.tsx
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+ 
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text>Content may vary</Text>
+      </View>
+  </View>
+  );
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'blue',
+     flex: 1,
+  
+    justifyContent: 'center',
+   
+     alignItems: 'center',
+  },
+
+  box: {
+   minHeight: 160, // Minimum height set to 160 pixels
+   minWidth: 200,
+  
+  padding: 10,
+   backgroundColor: 'red',
+   borderWidth: 1,
+   borderColor: '#ccc',
+   justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+ 
+  
+});
+
+export default App;
+
+//https://chat.openai.com/c/ad5080fe-ab1c-4622-bffc-90e185b7de7f
