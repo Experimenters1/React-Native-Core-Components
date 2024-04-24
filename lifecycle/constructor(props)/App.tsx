e1a86dab026b137988fbeb587c578e0868bc2be1
Huy vu <huyvu@Huys-MacBook-Pro.local>
2024-04-24 15:11:17 +0700

    Second commit

---
+++ lifecycle/constructor(props)/App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React ,{useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text
+
+} from 'react-native';
+
+import Welcome from './Component/Welcome';
+
+
+function App(): React.JSX.Element {
+ 
+  return (
+    <View style={styles.container}>
+        <Welcome name='huy' /> 
+        <Welcome name='hoang' /> 
+        <Welcome name='hoa' /> 
+    </View>
+  );
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fcba03'
+  },
+
+
+ 
+  
+});
+
+export default App;
+
+//https://chat.openai.com/c/ad5080fe-ab1c-4622-bffc-90e185b7de7f
